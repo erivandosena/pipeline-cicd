af6a09c405486eb92114075852dcec4de0feb9da
Erivando Sena <erivandosena@gmail.com>
2023-02-28 02:33:31 -0300

    Update ApplicationTests.java

--- spring-boot/src/test/java/br/edu/unilab/app/ApplicationTests.java
+++ spring-boot/src/test/java/br/edu/unilab/app/ApplicationTests.java
@@ -4,13 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import br.edu.unilab.app.Application;
 
-@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
