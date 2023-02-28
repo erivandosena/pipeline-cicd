743558bd50f839823d14a534d5c2abae015980a6
Erivando Sena <erivandosena@gmail.com>
2023-02-28 01:45:19 -0300

    Update ApplicationTests.java

--- spring-boot/src/test/java/br/edu/unilab/app/ApplicationTests.java
+++ spring-boot/src/test/java/br/edu/unilab/app/ApplicationTests.java
@@ -4,15 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import br.edu.unilab.app.Application;
 
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads(ApplicationContext context) {
 		assertThat(context).isNotNull();
 	}
 
