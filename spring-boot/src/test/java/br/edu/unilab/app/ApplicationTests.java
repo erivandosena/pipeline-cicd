ba67b9149fdda9e2fda0070e2a1dbeb9f9955760
Erivando Sena <erivandosena@gmail.com>
2023-02-28 02:27:45 -0300

    Update ApplicationTests.java

--- spring-boot/src/test/java/br/edu/unilab/app/ApplicationTests.java
+++ spring-boot/src/test/java/br/edu/unilab/app/ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
